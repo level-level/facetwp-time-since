e7a7ae604c88cf29ac2c34a67de1164e697d31e9
mgibbs189 <mgibbs189@gmail.com>
2016-06-15 16:41:12 -0400

    Tagged 1.3

--- facetwp-time-since.php
+++ facetwp-time-since.php
@@ -3,30 +3,14 @@
 Plugin Name: FacetWP - Time Since
 Plugin URI: https://facetwp.com/
 Description: "Time Since" facet
-Version: 1.2.1
+Version: 1.3
 Author: FacetWP, LLC
-Author URI: https://facetwp.com/
-GitHub Plugin URI: https://github.com/FacetWP/facetwp-time-since
-
-Copyright 2016 FacetWP, LLC
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
+GitHub URI: facetwp/facetwp-time-since
 */
 
-// exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+defined( 'ABSPATH' ) or exit;
 
+include( dirname( __FILE__ ) . '/github-updater.php' );
 
 /**
  * WordPress init hook
