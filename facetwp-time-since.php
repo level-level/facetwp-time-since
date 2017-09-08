a8da8459619202f412c7f17bc4a96a80c75ea274
mgibbs189 <mgibbs189@gmail.com>
2017-09-07 21:09:13 -0400

    Tagged 1.4

--- facetwp-time-since.php
+++ facetwp-time-since.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: FacetWP - Time Since
 Description: "Time Since" facet
-Version: 1.3.3
+Version: 1.4
 Author: FacetWP, LLC
 Author URI: https://facetwp.com/
 GitHub URI: facetwp/facetwp-time-since
